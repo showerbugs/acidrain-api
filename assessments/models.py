478b3f05bdc5c18dc2006ce916defbc90ca998f0
qodot <waitingforqodot@gmail.com>
2017-08-05 17:48:46 +0900

    Add models, migration scripts

--- assessments/models.py
+++ assessments/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db.models import Model
+from django.db.models import CharField
+from django.db.models import ForeignKey
+from django.db.models import DateTimeField
+from django.db.models import IntegerField
 
-# Create your models here.
+
+class Assessment(Model):
+    type = CharField(max_length=10)  # word, sentence
+    total = IntegerField()
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+
+class History(Model):
+    assessment = ForeignKey('Assessment', related_name='histories')
+    score = IntegerField()
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
--- sentences/models.py
+++ sentences/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db.models import Model
+from django.db.models import CharField
+from django.db.models import TextField
+from django.db.models import DateTimeField
+from django.db.models import IntegerField
 
-# Create your models here.
+
+class Sentence(Model):
+    type = CharField(max_length=10)  # word, sentence
+    body = TextField()
+    difficulty = IntegerField()
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
